7105ea8d819be646d917b5efa717402b1702f42e
sherif-omar <mr.sherifomar@hotmail.com>
2018-03-26 01:34:58 +0200

    Update stock.rb

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -7,8 +7,8 @@ class Stock < ActiveRecord::Base
 
     def self.new_from_lookup(ticker_symbol)
         looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-        return nil unless looked_up_stock.name
-        new_stock = new(ticker: looked_up_stock.symbol, name: looked_up_stock.name)
+        return nil unless looked_up_stock.company_name
+        new_stock = new(ticker: looked_up_stock.symbol, name: looked_up_stock.company_name)
         new_stock.last_price = new_stock.price
         new_stock
     end
